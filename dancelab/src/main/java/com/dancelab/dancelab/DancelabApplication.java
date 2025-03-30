2b7c09b79f7b6df645fd8539c603dd0634eaf2b7
Peshala G A K J <jayanikalansooriya24@gmail.com>
2025-03-30 13:00:50 +0530

    first commit

---
+++ dancelab/src/main/java/com/dancelab/dancelab/DancelabApplication.java
@@ -0,0 +1,13 @@
+package com.dancelab.dancelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancelabApplication.class, args);
+	}
+
+}
